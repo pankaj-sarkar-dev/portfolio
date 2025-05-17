ea4ef2b870bb4a40f459a6114a45a975d4779a3c
Pankaj-Sarkar <pankaj.sarkar.dev@gmail.com>
2025-05-17 15:18:39 +0530

    added hero section

--- app.js
+++ app.js
@@ -30,3 +30,72 @@ document.getElementById("cards").onmousemove = (e) => {
     card.style.setProperty("--mouse-y", `${y}px`);
   }
 };
+
+const texts = ["developer", "engineer"];
+
+let current = 0;
+const textBox = document.getElementById("textBox");
+
+function typeText(text, i = 0) {
+  if (i === 0) {
+    textBox.textContent = "";
+  }
+
+  if (i < text.length) {
+    textBox.textContent += text.charAt(i);
+    setTimeout(() => typeText(text, i + 1), 60);
+  } else {
+    setTimeout(() => {
+      textBox.classList.remove("blinking");
+      textBox.classList.add("zoom-blur-out");
+
+      setTimeout(() => {
+        current = (current + 1) % texts.length;
+        textBox.classList.remove("zoom-blur-out");
+        textBox.classList.add("blinking");
+        typeText(texts[current]);
+      }, 800); // after zoom-blur
+    }, 1500); // wait before blur
+  }
+}
+
+typeText(texts[current]);
+
+document.querySelectorAll(".button-wrapper").forEach((wrapper) => {
+  const button = wrapper.querySelector(".glow-button");
+  const glow = wrapper.querySelector(".glow");
+
+  wrapper.addEventListener("mousemove", (e) => {
+    const rect = wrapper.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    wrapper.style.setProperty("--mouse-x", `${x}px`);
+    wrapper.style.setProperty("--mouse-y", `${y}px`);
+    glow.style.left = `${x}px`;
+  });
+
+  wrapper.addEventListener("mouseleave", () => {
+    wrapper.style.setProperty("--mouse-x", `100%`);
+    wrapper.style.setProperty("--mouse-y", `50%`);
+    glow.style.left = `100%`;
+  });
+
+  // Handle smooth scroll on click
+  button.addEventListener("click", (e) => {
+    // console.log(clicked);
+    const targetSelector = button.getAttribute("data-target");
+    const target = document.querySelector(targetSelector);
+    if (target) {
+      e.preventDefault(); // Prevent default button behavior
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
+
+const marqueeTrack = document.getElementById("marquee-track");
+const text = marqueeTrack.children[0].outerHTML;
+
+while (marqueeTrack.scrollWidth < marqueeTrack.parentElement.offsetWidth * 2) {
+  marqueeTrack.insertAdjacentHTML("beforeend", text);
+}
