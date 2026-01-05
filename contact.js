67bfaade691ee80f1d1f0d63d897e0e6162dc0ce
Pankaj-Sarkar <pankaj.sarkar.dev@gmail.com>
2026-01-05 16:12:25 +0530

    some changes in form

--- contact.js
+++ contact.js
@@ -9,3 +9,15 @@ document
       body: formData,
     });
   });
+
+document
+  .getElementById("contact-button")
+  .addEventListener("click", async (e) => {
+    e.preventDefault();
+    const form = document.getElementById("contact-form");
+    const formData = new FormData(form);
+    await fetch("https://formsubmit.co/pankaj.sarkar.dev@gmail.com", {
+      method: "POST",
+      body: formData,
+    });
+  });
